8ddcfd9fdea60226deda2b7211fa8007585cd2e1
gjgustjd <gjgustjd70@naver.com>
2022-02-19 02:05:43 +0900

    :construction: Feat: 채팅방에 상대방 이름 매칭
    채팅방에 상대방 이름 매칭

--- app/src/main/java/com/miso/chatapplication/main/RecyclerChatRoomsAdapter.kt
+++ app/src/main/java/com/miso/chatapplication/main/RecyclerChatRoomsAdapter.kt
@@ -13,6 +13,7 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.getValue
 import com.miso.chatapplication.main.MainActivity
 import com.miso.chatapplication.R
+import com.miso.chatapplication.databinding.ListChatroomItemBinding
 import com.miso.chatapplication.databinding.ListPersonItemBinding
 import com.miso.chatapplication.model.ChatRoom
 import com.miso.chatapplication.model.User
@@ -20,13 +21,13 @@ import com.miso.chatapplication.model.User
 class RecyclerChatRoomsAdapter(val context: Context) :
     RecyclerView.Adapter<RecyclerChatRoomsAdapter.ViewHolder>() {
     var chatRooms: ArrayList<ChatRoom> = arrayListOf()
+    val myUid = FirebaseAuth.getInstance().currentUser?.uid.toString()
 
     init {
         setupAllUserList()
     }
 
     fun setupAllUserList() {
-        val myUid = FirebaseAuth.getInstance().currentUser?.uid.toString()
 
         FirebaseDatabase.getInstance().getReference("ChatRoom").child("chatRooms")
             .orderByChild("users/$myUid").equalTo(true)
@@ -45,11 +46,22 @@ class RecyclerChatRoomsAdapter(val context: Context) :
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.list_person_item, parent, false)
-        return ViewHolder(ListPersonItemBinding.bind(view))
+        val view = LayoutInflater.from(context).inflate(R.layout.list_chatroom_item, parent, false)
+        return ViewHolder(ListChatroomItemBinding.bind(view))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        var userIdList = chatRooms[position].users!!.keys
+        var opponent = userIdList.first{!it.equals(myUid)}
+        FirebaseDatabase.getInstance().getReference("User").child("users").orderByChild("uid").equalTo(opponent)
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onCancelled(error: DatabaseError) {}
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    for (data in snapshot.children) {
+                        holder.txt_name.text = data.getValue<User>()!!.name.toString()
+                    }
+                }
+            })
     }
 
 
@@ -58,11 +70,13 @@ class RecyclerChatRoomsAdapter(val context: Context) :
     }
 
 
-    inner class ViewHolder(itemView: ListPersonItemBinding) :
+    inner class ViewHolder(itemView: ListChatroomItemBinding) :
         RecyclerView.ViewHolder(itemView.root) {
         var background = itemView.background
         var txt_name = itemView.txtName
-        var txt_email = itemView.txtEmail
+        var txt_message = itemView.txtMessage
+        var txt_date = itemView.txtMessageDate
+        var txt_chatCount = itemView.txtChatCount
     }
 
 }
