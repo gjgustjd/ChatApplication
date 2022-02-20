18f34667316cb7233506cb8d1ac51502ea4c1eaa
gjgustjd <gjgustjd70@naver.com>
2022-02-20 15:24:16 +0900

    :recycle: Refactor: ChatRoomActivity.kt  예외처리
    예외처리

--- app/src/main/java/com/miso/chatapplication/chatroom/ChatRoomActivity.kt
+++ app/src/main/java/com/miso/chatapplication/chatroom/ChatRoomActivity.kt
@@ -22,6 +22,7 @@ import com.miso.chatapplication.model.User
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.*
+import kotlin.Exception
 
 @RequiresApi(Build.VERSION_CODES.O)
 class ChatRoomActivity : AppCompatActivity() {
@@ -35,39 +36,50 @@ class ChatRoomActivity : AppCompatActivity() {
     lateinit var chatRoom: ChatRoom
     lateinit var opponentUser: User
     lateinit var chatRoomKey: String
+    lateinit var myUid: String
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityChatroomBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initializeProperty()
         initializeView()
+        initializeListener()
+        setupChatRooms()
     }
 
-    fun initializeView() {
+    fun initializeProperty() {
+        myUid = FirebaseAuth.getInstance().currentUser?.uid!!
+        firebaseDatabase = FirebaseDatabase.getInstance().reference!!
+
         chatRoom = (intent.getSerializableExtra("ChatRoom")) as ChatRoom
         chatRoomKey = intent.getStringExtra("ChatRoomKey")!!
         opponentUser = (intent.getSerializableExtra("Opponent")) as User
+    }
 
-        Log.i("opponentUser", opponentUser.name.toString())
-        Log.i("opponentUser", opponentUser.uid.toString())
-        var myUid = FirebaseAuth.getInstance().currentUser?.uid
-        firebaseDatabase = FirebaseDatabase.getInstance().reference!!
+    fun initializeView() {
         btn_exit = binding.imgbtnQuit
-        btn_exit.setOnClickListener()
-        {
-            startActivity(Intent(this@ChatRoomActivity, MainActivity::class.java))
-        }
         edt_message = binding.edtMessage
         recycler_talks = binding.recyclerMessages
         btn_submit = binding.btnSubmit
         txt_title = binding.txtTItle
         txt_title.text = opponentUser!!.name ?: ""
+    }
+
+    fun initializeListener() {
+        btn_exit.setOnClickListener()
+        {
+            startActivity(Intent(this@ChatRoomActivity, MainActivity::class.java))
+        }
         btn_submit.setOnClickListener()
         {
             putMessage()
         }
+    }
 
-        if(chatRoomKey.isNullOrBlank())
+    fun setupChatRooms() {
+        if (chatRoomKey.isNullOrBlank())
             setupChatRoomKey()
         else
             setupRecycler()
@@ -87,24 +99,39 @@ class ChatRoomActivity : AppCompatActivity() {
                 }
             })
     }
+
     fun putMessage() {
-        var myUid = FirebaseAuth.getInstance().currentUser!!.uid
-        var localDateTime = LocalDateTime.now()
-        localDateTime.atZone( TimeZone.getDefault().toZoneId())
-        var dateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss")
-        var dateTimeString = localDateTime.format(dateTimeFormatter).toString()
-        var message = Message(myUid, dateTimeString, edt_message.text.toString())
-        Log.i("ChatRoomKey",chatRoomKey)
-        FirebaseDatabase.getInstance().getReference("ChatRoom").
-        child("chatRooms").child(chatRoomKey).child("messages")
-            .push().setValue(message).addOnSuccessListener {
-            Log.i("putMessage", "메시지 전송에 성공하였습니다.")
-            edt_message.text.clear()
+        try {
+            var message = Message(myUid, getDateTimeString(), edt_message.text.toString())
+            Log.i("ChatRoomKey", chatRoomKey)
+            FirebaseDatabase.getInstance().getReference("ChatRoom").child("chatRooms")
+                .child(chatRoomKey).child("messages")
+                .push().setValue(message).addOnSuccessListener {
+                    Log.i("putMessage", "메시지 전송에 성공하였습니다.")
+                    edt_message.text.clear()
+                }.addOnCanceledListener {
+                    Log.i("putMessage", "메시지 전송에 실패하였습니다")
+                }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.i("putMessage", "메시지 전송 중 오류가 발생하였습니다.")
+        }
+    }
+
+    fun getDateTimeString(): String {
+        try {
+            var localDateTime = LocalDateTime.now()
+            localDateTime.atZone(TimeZone.getDefault().toZoneId())
+            var dateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss")
+            return localDateTime.format(dateTimeFormatter).toString()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            throw Exception("getTimeError")
         }
     }
 
     fun setupRecycler() {
         recycler_talks.layoutManager = LinearLayoutManager(this)
-        recycler_talks.adapter = RecyclerMessagesAdapter(this,chatRoomKey,opponentUser.uid)
+        recycler_talks.adapter = RecyclerMessagesAdapter(this, chatRoomKey, opponentUser.uid)
     }
 }
