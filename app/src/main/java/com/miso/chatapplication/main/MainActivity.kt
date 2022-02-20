79218a773139e261de33c4bce6b6fa99937762af
gjgustjd <gjgustjd70@naver.com>
2022-02-20 13:28:30 +0900

    :sparkles: Feat:back 버튼 누를 시 로그아웃 다이얼로그 표시
    Back 버튼 누를 시 로그아웃 다이얼로그 표시

--- app/src/main/java/com/miso/chatapplication/main/MainActivity.kt
+++ app/src/main/java/com/miso/chatapplication/main/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.miso.chatapplication.main
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -14,6 +17,7 @@ import com.miso.chatapplication.databinding.ActivityMainBinding
 import com.miso.chatapplication.model.ChatRoom
 
 
+@RequiresApi(Build.VERSION_CODES.O)
 class MainActivity : AppCompatActivity() {
     lateinit var btnAddchatRoom: Button
     lateinit var binding: ActivityMainBinding
@@ -38,16 +42,28 @@ class MainActivity : AppCompatActivity() {
         }
         recycler_chatroom = binding.recyclerChatrooms
     }
-    fun setupRecycler()
-    {
+
+    fun setupRecycler() {
         recycler_chatroom.layoutManager = LinearLayoutManager(this)
         recycler_chatroom.adapter = RecyclerChatRoomsAdapter(this)
 
     }
 
     override fun onBackPressed() {
-        super.onBackPressed()
-        startActivity(Intent(this@MainActivity, LoginActivity::class.java))
-        finish()
+        val builder = AlertDialog.Builder(this)
+            .setTitle("로그아웃")
+            .setMessage("로그아웃 하시겠습니까?")
+            .setPositiveButton("확인",
+                { dialog, id ->
+                    FirebaseAuth.getInstance().signOut()
+                    startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+                    dialog.dismiss()
+                    finish()
+                })
+            .setNegativeButton("취소",
+                {dialog,id->
+                    dialog.dismiss()
+                })
+        builder.show()
     }
 }
